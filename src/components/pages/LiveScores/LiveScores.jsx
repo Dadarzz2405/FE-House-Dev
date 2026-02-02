3d4c85c13a1f43f53d38de26a1859786fc6b8404
Dadarzz2405 <haidaralifawwaz@gmail.com>
2026-02-02 09:18:11 +0700

    toml and livescore

--- src/components/pages/LiveScores/LiveScores.jsx
+++ src/components/pages/LiveScores/LiveScores.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Table } from "react-bootstrap";
-import "./livescore.css";
+import "./LiveScore.css";
 
 const LiveScores = () => {
   const [housesRank, sethousesRank] = useState([]);
